7d932756723a0529084676b3988110586b12d677
Chirayu <chirayu47sharma@gmail.com>
2025-07-29 16:31:43 +0530

    CHANGE CNN

--- CNN.py
+++ CNN.py
@@ -13,6 +13,7 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 
 # device config
@@ -36,22 +37,44 @@ transforms = transforms.Compose(
 train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                              download=True, transform=transforms)
 
-test_dataset = torchvision.datasets.CIFAR10(root='/data', train=False,
-                                            download=False,transform=transforms)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                            transform=transforms)
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader  = DataLoader(dataset=test_dataset, shuffle=True, batch_size=batch_size)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog','horse','ship','truck')
+example = iter(train_loader)
+samples, label =next(example)
+
+# print(samples.shape, label.shape)
+for i in range(len(samples)):
+        plt.subplot(2,3,i+1)
+        plt.imshow(samples[i][0])
+plt.show()
 
 # implement conv net
+
 class ConvNet(nn.Module):
-        def __init__(self, *args, **kwargs):
-                super().__init__(*args, **kwargs)
+        def __init__(self):
+                super(ConvNet,self).__init__()
+                self.conv1 = nn.Conv2d(3,6,5)
+                self.pool = nn.MaxPool2d(2,2)
+                self.conv2 = nn.Conv2d(6,16,5)
+                self.fc1 = nn.Linear(16*5*5,120)
+                self.fc2 = nn.Linear(120,84)
+                self.fc3 = nn.Linear(84,10)
+                pass
                 
         def forward(self, x):
-                pass
+                x= self.pool(F.leaky_relu(self.conv1(x)))
+                x= self.pool(F.leaky_relu(self.conv2(x)))
+                x= x.view(-1, 16*5*5)
+                x= F.relu(self.fc1(x))
+                x= F.relu(self.fc2(x))
+                x= self.fc3(x)
+                return x
         
 model = ConvNet().to(device)
 
@@ -82,3 +105,32 @@ for epoch in range(num_epoch):
                         print(f'epoch [{epoch+1}/{num_epoch}], step[{i+1}/{n_total_steps}], [loss: {loss.item():.4f}]')
                         
 print("finish training")
+
+with torch.no_grad():
+        n_correct =0
+        n_samples =0
+        n_class_correct = [0 for i in range(10)]
+        n_class_samples = [0 for i in range(10)]
+        for image, labels in test_loader:
+                image = image.to(device)
+                label = labels.to(device)
+                output = model(image)
+                
+                # max returns (value, index)
+                _, predictions = torch.max(output,1)
+                n_samples = labels.size(0)
+                n_correct = (predictions==labels).sum().item()
+                
+                for i in range(batch_size):
+                        label= labels[i]
+                        pred = predictions[i]
+                        if (label == pred):
+                                n_class_correct[label] +=1
+                                n_class_samples[label] +=1
+                                
+        acc = 100.0 * n_correct /n_samples
+        print(f'Accuracy of the network:{acc}%')
+        
+        for i in range(10):
+                acc = 100.0 * n_class_correct[i] /n_class_samples[i]
+                print(f"Accuracy of {classes[i]}:{acc}%")
